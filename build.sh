5167ee127585b21bad8fb5a4fdbd70e2759a9dae
drdbrr <drdbrr@yandex.ru>
2023-08-18 06:57:39 +0200

    plain_commit

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-#clear && cd ./build && rm -rf * &&
-clear && cd ./build && cmake -DPython_EXECUTABLE=$(python3 -c "import sys; print(sys.executable)") .. && cmake  --build . -j 6
+clear
+if [ ! -d "./build" ]
+then
+    mkdir ./build
+fi
+cd ./build
+cmake -DPython_EXECUTABLE=$(python3 -c "import sys; print(sys.executable)") ..
+cmake  --build . -j 6
